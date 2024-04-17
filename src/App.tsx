08fc3dc7e34e1eb7f7d66a5e5e4b65d52c696ee7
Ron-Aran Paju <ron-aran.paju@ut.ee>
2024-04-18 01:40:11 +0300

    Css needs improving
    Css needs improving

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className='contactInfo'>
+          <div>
+            <h1>Media enquiries:</h1>
+            <p>press@tuum.com</p>
+          </div>
+          <div>
+            <h1>Career questions:</h1>
+            <p>careers@tuum.com</p>
+        </div>
+      </div>
         <ContactForm /> {/* use the ContactForm component */}
       </header>
     </div>
--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -17,25 +17,38 @@ const ContactForm = () => {
     };
   
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-row">
-            <input {...register('firstName')} placeholder="First name" />
-            <input {...register('lastName')} placeholder="Last name" />
-          </div>
-          <div className="form-row">
-            <input {...register('email')} placeholder="Email" />
-            <input {...register('jobTitle')} placeholder="Job title" />
-          </div>
-          <ReactFlagsSelect
-            selected={selected}
-            onSelect={handleSelect}
-          />
-          <input {...register('companyName')} placeholder="Company name" />
-          <div className="form-row">
-            <textarea {...register('message')} placeholder="What would you like to talk about?" style={{height: '100px'}} />
-          </div>
-          <input type="submit" />
-        </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Contact Us</h1>
+        <div className="form-row">
+          <input {...register('firstName')} placeholder="First name" />
+          <input {...register('lastName')} placeholder="Last name" />
+        </div>
+        <div className="form-row">
+          <input {...register('email')} placeholder="Email" />
+          <input {...register('jobTitle')} placeholder="Job title" />
+        </div>
+        <ReactFlagsSelect
+          selected={selected}
+          onSelect={handleSelect}
+        />
+        <input {...register('companyName')} placeholder="Company name" />
+        <div className="form-row">
+          <textarea {...register('message')} placeholder="What would you like to talk about?" style={{height: '100px'}} />
+        </div>
+        <div className="form-row">
+          <label>
+            <input type="checkbox" {...register('acceptPolicies')} />
+            By submitting this form I accept privacy policy and cookie policy. *
+          </label>
+        </div>
+        <div className="form-row">
+          <label>
+            <input type="checkbox" {...register('receiveNewsletter')} />
+            I would like to receive your newsletter.
+          </label>
+        </div>
+        <input type="submit" />
+      </form>
     );
   };
   
